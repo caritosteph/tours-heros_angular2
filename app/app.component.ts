8355e52cf2e60452c28a335edef84cd0579caf4f
Ulfe Urena, Carolyn S <carolyn.ulfe@one.verizon.com>
2016-11-25 18:53:33 -0500

    List of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
+import {Hero} from './home/Hero';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Dr.Strange' },
+  { id: 12, name: 'Spiderman' },
+  { id: 13, name: 'Wolverine' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Black Panter' },
+  { id: 16, name: 'Iroman' },
+  { id: 20, name: 'Howkeye' }
+];
+
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    <li *ngFor = "let hero of heroes">
+      <span>{{hero.id}}</span>{{hero.name}}
+    </li>
+  </ul>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Marvel Heroes';
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Spiderman'
+  };
+}
