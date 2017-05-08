5d9a2826247aa1778dee976aeb495e22cc4eee18
Ulfe Urena, Carolyn S <carolyn.ulfe@one.verizon.com>
2017-05-08 16:03:37 -0500

    adding multiple components

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './home/Hero';
+
+@Component({
+  selector: 'hero-detail',
+  template: `
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+    </div>
+  </div>
+  `
+})
+
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
