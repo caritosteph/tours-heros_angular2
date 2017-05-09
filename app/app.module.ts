4781ec693faa96086b0cbf3e3ee80b71bdf81242
Ulfe Urena, Carolyn S <carolyn.ulfe@one.verizon.com>
2017-05-09 14:45:12 -0500

    adding routers

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,30 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
